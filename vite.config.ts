64cc8ec548e779b9bf3fdf877a79850882fc92be
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2024-02-08 23:06:31 +0545

    build: disable vite server

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from '@ownclouders/extension-sdk'
 
 export default defineConfig({
-  name: 'web-3dmodel-viewer',
-  server: {
-    port: 3000
-  }
+  name: 'web-3dmodel-viewer'
 })
