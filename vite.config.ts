999a3f93a10c4ff5b02f33365fdab88a0cdc1292
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2024-02-09 19:28:43 +0545

    build: always build with static name

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from '@ownclouders/extension-sdk'
 
 export default defineConfig({
-  name: 'web-3dmodel-viewer'
+  name: 'web-3dmodel-viewer',
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'extension.js'
+      }
+    }
+  }
 })
