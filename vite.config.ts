74eefdda8cc94a5c9f63a2a4b273a967e3767938
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2024-02-18 17:41:24 +0545

    build: custom vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from '@ownclouders/extension-sdk'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  name: 'web-3dmodel-viewer',
   build: {
     rollupOptions: {
+      external: ['vue', '@ownclouders/web-client', '@ownclouders/web-pkg'],
+      input: 'src/index.ts',
       output: {
+        format: 'amd',
         dir: 'dist',
         entryFileNames: 'extension.js'
-      }
+      },
+      preserveEntrySignatures: 'strict'
     }
-  }
+  },
+  plugins: [
+    vue({
+      // set to true when switching to esm
+      customElement: false
+    })
+  ]
 })
