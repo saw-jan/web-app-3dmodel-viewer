704338de6780b39cc3f7204cbde58516d7180f9d
Kathrin Koebel <kathrin@koebel.ch>
2024-03-13 16:22:16 +0545

    add e2e config.js

---
+++ tests/e2e/config.js
@@ -0,0 +1,14 @@
+exports.config = {
+    // environment
+    baseUrlOcis: process.env.BASE_URL_OCIS ?? 'https://host.docker.internal:9200',
+    assets: './tests/e2e/filesForUpload',
+    adminUser: process.env.ADMIN_USERNAME || 'admin',
+    adminPassword: process.env.ADMIN_PASSWORD || 'admin',
+    // cucumber
+    retry: process.env.RETRY || 0,
+    // playwright
+    slowMo: parseInt(process.env.SLOW_MO) || 0,
+    timeout: parseInt(process.env.TIMEOUT) || 60,
+    minTimeout: parseInt(process.env.MIN_TIMEOUT) || 5,
+    headless: process.env.HEADLESS === 'true'
+}
