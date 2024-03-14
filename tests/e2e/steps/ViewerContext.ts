488d0c25eee42a2376f012a5dd13c598bbd5d131
Kathrin Koebel <kathrin@koebel.ch>
2024-03-14 13:01:55 +0545

    update feature file and steps

--- tests/e2e/steps/ViewerContext.ts
+++ tests/e2e/steps/ViewerContext.ts
@@ -6,7 +6,7 @@ import { Viewer } from '../pageObjects/Viewer'
 import { getUser } from '../userStore'
 
 
-Given('the user has logged in with username {string} and password {string}', function (string, string2) {
+Given('the user has logged in with username {string} and password {string}', function (username, password) {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
 })
@@ -31,31 +31,33 @@ Given('the user has uploaded the following 3D models', function () {
 // from dicom viewer e2e test
 Given('the user has uploaded the dicom file {string}', async function (filename: string): Promise<void> {
     const viewer = new Viewer()
-    await viewer.upload({ filename })
+    await viewer.uploadFile({ filename })
 })
 */
 
 When('the user previews the file {string} in the 3D model viewer', async function (filename: string): Promise<void> {
     const viewer = new Viewer()
-    //await viewer.previewFile({ filename })
+    await viewer.previewFile({ filename })
 })
 
-Then('the 3D model {string} should be displayed in the viewport', function (string) {
+Then('the 3D model {string} should be displayed in the viewport', async function (filename: string): Promise<void> {
+    // Write code here that turns the phrase above into concrete actions
+    const viewer = new Viewer()
+    await viewer.checkViewport({ filename })
+})
+
+Then('the file name {string} should be shown in the topbar', async function (filename: string): Promise<void> {
+    // Write code here that turns the phrase above into concrete actions
+    const viewer = new Viewer()
+    await viewer.checkFileName({ filename })
+})
+
+When('the user enters fullscreen mode"', function () {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
 })
 
-Then('the file name {string} should be shown in the topbar', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-})
-
-When('the user selects "Enter full screen mode"', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-})
-
-Then('the 3D model should be displayed in full screen mode', function () {
+Then('the 3D model should be displayed in fullscreen mode', function () {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
 })
@@ -65,7 +67,7 @@ Then('the topbar should not be visible', function () {
     return 'pending';
 })
 
-When('the user selects "Exit full screen mode"', function () {
+When('the user exits fullscreen mode', function () {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
 })
@@ -91,7 +93,7 @@ Then('the size and position of the 3D model will be changed accordingly', functi
     return 'pending';
 });
 
-When('the user selects "Reset"', function () {
+When('the user resets the viewport', function () {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
 })
@@ -101,14 +103,12 @@ Then('the 3D model should be display in the default size and position', function
     return 'pending';
 })
 
-When('the user selects "Show next model"', function () {
+When('the user navigates to the next model', function () {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
 })
 
-When('the user selects "Show previous model"', function () {
+When('the user navigates to the previous model', function () {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
 })
-
-
