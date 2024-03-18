f9d9c1f689b3e23c0b8eae86f50255312693c475
Kathrin Koebel <kathrin@koebel.ch>
2024-03-18 16:06:27 +0545

    implement first 5 steps of the scenario & adding some notes for checking mouse input

--- tests/e2e/pageObjects/Viewer.ts
+++ tests/e2e/pageObjects/Viewer.ts
@@ -1,7 +1,7 @@
 import { Page, expect } from '@playwright/test'
 import { state } from '../hooks'
 import { config } from '../config.js'
-// import util from 'util' // check import
+import util from 'util'
 
 export class Viewer {
     page: Page = state.page
@@ -21,6 +21,8 @@ export class Viewer {
         appTopBar : '.oc-app-top-bar',
         appTopBarResourceName : '.oc-resource-name',
         modelViewport : '#preview .model-viewport',
+        modelViewportDescription : '#preview h1.oc-invisible-sr',
+        modelViewportCanvas : '#preview .model-viewport canvas',
         controlButtonPrev : '.preview-controls-previous',
         controlButtonNext : '.preview-controls-next',
         controlButtonFullscreen : '.preview-controls-fullscreen',
@@ -43,21 +45,29 @@ export class Viewer {
         await this.page.locator(this.elements.resourceUploadButton).click()
         await this.page.locator(this.elements.fileUploadInput).setInputFiles(`${config.assets}/${filename}`)
         await this.page.locator(this.elements.uploadInfoCloseButton).click()
-        // util
-        // await this.page.locator(util.format(this.elements.resourceNameSelector, filename)).waitFor()
+        await this.page.locator(util.format(this.elements.resourceNameSelector, filename)).waitFor()
     }
 
     async previewFile(filename: string): Promise<void> {
-        // await this.page.locator(util.format(this.elements.resourceNameSelector, filename)).click()
-        // await expect(this.page.locator(this.elements.modelViewport)).toBeVisible()
+        await this.page.locator(util.format(this.elements.resourceNameSelector, filename)).click()
+        await expect(this.page.locator(this.elements.modelViewport)).toBeVisible()
     }
     
     async checkViewport(filename: string): Promise<void> {
         await expect(this.page.locator(this.elements.modelViewport)).toBeVisible()
+        await expect(this.page.locator(this.elements.modelViewportCanvas)).toBeVisible()
+        // check if file name is displayed in hidden h1 title element
+        const description = await this.page.locator(this.elements.modelViewportDescription).innerText()
+        await expect(description).toContain(filename)
+        // todo: check if there are better ways to verify if the model is displayed in the viewport
     }
 
     async checkFileName(filename: string): Promise<void> {
-        //await expect(this.page.locator(util.format(this.elements.appbarResourceNameSelector, filename))).toBeVisible()
+        // from dicom viewer e2e test
+        await expect(this.page.locator(util.format(this.elements.appbarResourceNameSelector, filename))).toBeVisible()
+        // own approach for checking file name
+        const appTopBarResourceName = await this.page.locator(this.elements.appTopBarResourceName).innerText()
+        await expect(appTopBarResourceName.replace(/(\r\n|\n|\r)/gm,'').trim()).toContain(filename)
     }
 
     async checkTopbarVisibility( shouldBeVisible: Boolean ): Promise<void> {
@@ -86,5 +96,40 @@ export class Viewer {
 
     async displayPreviousModel(): Promise<void> {
         await this.page.locator(this.elements.controlButtonPrev).click()
+    }
+
+    async modifyModel(): Promise<void> {
+        // select viewport
+        // move or wheel mouse interaction
+        //
+
+        // await this.page.locator(this.elements.??).mouse.move()
+        // mousewheel?
+        // https://www.lambdatest.com/automation-testing-advisor/javascript/playwright-internal-mouseWheel
+
+        /*
+        https://playwright.dev/python/docs/api/class-mouse
+        mouse.wheel(delta_x, delta_y)
+        - delta_x (float#): Pixels to scroll horizontally
+        - delta_y (float#): Pixels to scroll vertically
+
+        await this.page.locator(this.elements.??).mouse.wheel()
+        */
+
+        /*
+        https://webscraping.ai/faq/playwright/how-to-handle-mouse-actions-using-playwright
+        await this.page.locator().mouse.move(startX, startY)
+        await this.page.mouse.down()
+        await this.page.mouse.move(endX, endY)
+        await this.page.mouse.up()
+        */
+
+        /*
+        https://playwright.dev/docs/input#mouse-click
+        await page.locator('#item-to-be-dragged').hover();
+        await page.mouse.down();
+        await page.locator('#item-to-drop-at').hover();
+        await page.mouse.up();
+        */
     }
 }
