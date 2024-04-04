fdc73d852ff08de8ce5c7b1a78e30d5e20d508e1
Kathrin Koebel <kathrin@koebel.ch>
2024-04-04 15:22:10 +0545

    refactoring deleteAllFiles()

--- tests/e2e/utils/helpers.ts
+++ tests/e2e/utils/helpers.ts
@@ -3,26 +3,32 @@ import { xml2js } from 'xml-js'
 import { _ } from 'lodash'
 import config from '../config'
 
+const _path = require('path')
+
 const sendRequest = ({ method, path }): Promise<any> => {
   const headers = {
     Authorization: `Basic ${Buffer.from(`${config.adminUser}:${config.adminPassword}`).toString('base64')}`
   }
   return axios({
     method,
-    url: path.join(config.baseUrlOcis, path),
+    url: _path.join(config.baseUrlOcis, path),
     headers
   })
 }
 
+const deleteFile = async (resource): Promise<void> => {
+  const href = _.get(resource, 'd:href._text')
+  return await sendRequest({ method: 'DELETE', path: href })
+}
+
 export const deleteAllFiles = async (): Promise<void> => {
-  // check path, it doesn't seem like this is deleting all files
   const response = await sendRequest({ method: 'PROPFIND', path: 'remote.php/dav/files/admin' })
   const xmlResponse = response.data
   const result = xml2js(xmlResponse, { compact: true })
   const resp = _.get(result, 'd:multistatus.d:response')
-  const href = _.get(resp[1], 'd:href._text')
-
-  return await sendRequest({ method: 'DELETE', path: href })
+  for (const r in resp) {
+    await deleteFile(r)
+  }
 }
 
 export const emptyTrashbin = async (): Promise<void> => {
